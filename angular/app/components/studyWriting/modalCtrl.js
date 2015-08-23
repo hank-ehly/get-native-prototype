5bf3270276d7142c290c1f67ebafaa7a36731b6f
Hank Ehly <hank.ehly@mulodo.com>
2015-08-23 13:29:22 +0900

    writing modal

---
+++ angular/app/components/studyWriting/modalCtrl.js
@@ -0,0 +1,24 @@
+(function() {
+  'use strict';
+
+  var modalCtrl = function($scope, answer, $modalInstance) {
+
+  	$scope.answer = answer;
+
+  	$scope.ok = function () {
+	    $modalInstance.close(answer);
+	  };
+
+	  $scope.cancel = function () {
+	    $modalInstance.dismiss('cancel');
+	  };
+
+  };
+
+  angular
+    .module('angularApp')
+    .controller('modalCtrl', modalCtrl);
+
+  modalCtrl.$inject = ['$scope', 'answer', '$modalInstance'];
+
+})();
